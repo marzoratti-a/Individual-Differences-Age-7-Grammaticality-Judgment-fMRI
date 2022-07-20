2b0b6bdc3dd6c02effb1e5fbffe7269e93ee01ec
marzoratti-a <107959811+marzoratti-a@users.noreply.github.com>
2022-07-19 21:09:00 -0400

    Update get_Confounds.sh

--- get_Confounds.sh
+++ get_Confounds.sh
@@ -1,25 +1,34 @@
 #!/bin/bash
 
-cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload
-
-#Check whether the file subjList.txt exists; if not, create it
+# Check whether the file subjList.txt exists; if not, create it
 if [ ! -f subjList.txt ]; then
 	ls | grep ^sub- > subjList.txt
 fi
 
-for subj in `cat subjList.txt`; do
-	cd $subj
-  # Make backup versions of regressor/confound files
-  cp /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/$subj/ses-7/notspecific/*sem_regressors.tsv /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/$subj_ses-7_sem_regressors.tsv 
-  cp /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/$subj/ses-7/notspecific/*plaus_regressors.tsv /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/$subj_ses-7_plaus_regressors.tsv 
-  cp /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/$subj/ses-7/notspecific/*gram_regressors.tsv /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/$subj_ses-7_gram_regressors.tsv
-  
- for reg in global_signal csf white_matter trans_x trans_y trans_z rot_x rot_y rot_z; do
-  for task in sem plaus gram; do
-    awk -v col=$reg 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' ${subj}_ses-7_${task}_regressors.tsv > ${reg}_${task}_all_tmp.txt;
-    sed '1d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_noHead_tmp.txt
-    sed '1!d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_Head_tmp.txt
-  done
-done
+# Check whether regressors folder exists; if not, create it
+if [ ! -d "/mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/regressors" ]; then
+	mkdir regressors
+fi
 
+for subj in `cat subjList.txt`; do
+  cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/$subj/ses-7/notspecific/
+  
+  # Make backup versions of regressor/confound files
+  cp *sem_regressors.tsv /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/regressors/$subj_ses-7_sem_regressors.tsv 
+  cp *plaus_regressors.tsv /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/regressors/$subj_ses-7_plaus_regressors.tsv 
+  cp *gram_regressors.tsv /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/regressors/$subj_ses-7_gram_regressors.tsv
+  
+  cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/regressors
+  
+  # Make txt files with and without header for each regressor of interest
+  for reg in global_signal csf white_matter trans_x trans_y trans_z rot_x rot_y rot_z; do
+	  for task in sem plaus gram; do
+	    awk -v col=$reg 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' ${subj}_ses-7_${task}_regressors.tsv > ${reg}_${task}_all_tmp.txt;
+	    sed '1d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_noHead_tmp.txt
+	    sed '1!d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_Head_tmp.txt
+	  done
+  done
+  
+  cd ../..
+  
 done
