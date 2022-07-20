a30fdd0cb4fa9d7b1d5cd87272ec1f3905484689
marzoratti-a <107959811+marzoratti-a@users.noreply.github.com>
2022-07-20 10:29:44 -0400

    Update get_Confounds.sh

--- get_Confounds.sh
+++ get_Confounds.sh
@@ -16,7 +16,7 @@ for subj in `cat subjList.txt`; do
   # Make txt files with and without header for each regressor of interest
   for reg in global_signal csf white_matter trans_x trans_y trans_z rot_x rot_y rot_z; do
 	  for task in sem plaus gram; do
-	    awk -v col=$reg 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' ${subj}_ses-7_${task}_regressors.tsv > ${reg}_${task}_all_tmp.txt;
+	    awk -v col=$reg 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' *_${task}_regressors.tsv > ${reg}_${task}_all_tmp.txt;
 	    sed '1d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_noHead_tmp.txt
 	    sed '1!d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_Head_tmp.txt
 	  done
