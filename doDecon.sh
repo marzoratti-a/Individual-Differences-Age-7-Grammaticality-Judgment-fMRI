97e14012cd1aff3e0454d639a16b35d48306092c
marzoratti-a <107959811+marzoratti-a@users.noreply.github.com>
2022-07-22 17:43:52 -0400

    Update doDecon.sh

--- doDecon.sh
+++ doDecon.sh
@@ -17,20 +17,20 @@ for task in sem plaus gram; do
     -mask full_mask.nii						     \
     -polort 1                                                                \
     -xout -progress                                                     \
-    -num_stimts 12                                                           \
+    -num_stimts 11                                                           \
     -stim_times 1 stimuli/${task}.1D 'BLOCK(2,1)'                          \
     -stim_label 1 ${task}                                                  \
     -stim_times 2 stimuli/${task}_ctrl.1D 'BLOCK(2,1)'                        \
     -stim_label 2 ${task}_ctrl                                                \
-    -stim_file 4 regressors/trans_x_${task}_all_tmp.txt'[0]' -stim_base 4 -stim_label 4 trans_x_${task}   \
-    -stim_file 5 regressors/trans_y_${task}_all_tmp.txt'[0]' -stim_base 5 -stim_label 5 trans_y_${task}   \
-    -stim_file 6 regressors/trans_z_${task}_all_tmp.txt'[0]' -stim_base 6 -stim_label 6 trans_z_${task}   \
-    -stim_file 7 regressors/rot_x_${task}_all_tmp.txt'[0]' -stim_base 7 -stim_label 7 rot_x_${task}     \
-    -stim_file 8 regressors/rot_y_${task}_all_tmp.txt'[0]' -stim_base 8 -stim_label 8 rot_y_${task}    \
-    -stim_file 9 regressors/rot_z_${task}_all_tmp.txt'[0]' -stim_base 9 -stim_label 9 rot_z_${task}    \
-    -stim_file 10 regressors/global_signal_${task}_all_tmp.txt'[0]' -stim_base 10 -stim_label 10 global_signal_${task}     \
-    -stim_file 11 regressors/csf_${task}_all_tmp.txt'[0]' -stim_base 11 -stim_label 11 csf_sem     \
-    -stim_file 12 regressors/white_matter_${task}_all_tmp.txt'[0]' -stim_base 12 -stim_label 12 white_matter_${task}     \
+    -stim_file 3 regressors/trans_x_${task}_noHead_tmp.txt'[0]' -stim_base 3 -stim_label 3 trans_x_${task}   \
+    -stim_file 4 regressors/trans_y_${task}_noHead_tmp.txt'[0]' -stim_base 4 -stim_label 4 trans_y_${task}   \
+    -stim_file 5 regressors/trans_z_${task}_noHead_tmp.txt'[0]' -stim_base 5 -stim_label 5 trans_z_${task}   \
+    -stim_file 6 regressors/rot_x_${task}_noHead_tmp.txt'[0]' -stim_base 6 -stim_label 6 rot_x_${task}     \
+    -stim_file 7 regressors/rot_y_${task}_noHead_tmp.txt'[0]' -stim_base 7 -stim_label 7 rot_y_${task}    \
+    -stim_file 8 regressors/rot_z_${task}_noHead_tmp.txt'[0]' -stim_base 8 -stim_label 8 rot_z_${task}    \
+    -stim_file 9 regressors/global_signal_${task}_noHead_tmp.txt'[0]' -stim_base 9 -stim_label 9 global_signal_${task}     \
+    -stim_file 10 regressors/csf_${task}_noHead_tmp.txt'[0]' -stim_base 10 -stim_label 10 csf_sem     \
+    -stim_file 11 regressors/white_matter_${task}_noHead_tmp.txt'[0]' -stim_base 11 -stim_label 11 white_matter_${task}     \
     -jobs 8  \
     # tells AFNI to run GLT based on label names
     -gltsym 'SYM: ${task} -${task}_ctrl'				     \
