b20ea4cc6248efb2b05e047cadbdf3c837125a29
marzoratti-a <107959811+marzoratti-a@users.noreply.github.com>
2022-07-22 17:12:00 -0400

    Update doDecon.sh

--- doDecon.sh
+++ doDecon.sh
@@ -2,9 +2,7 @@
 
 # first cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload
 for subj in `cat subjList.txt`; do
-cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/output/$subj/ses-7/func
-
-for task in sem plaus gram; do
+cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/$subj/ses-7/func
 
 if ( $#argv > 0 ) then
     set subj = $argv[1]
@@ -12,17 +10,18 @@ else
     set subj = s01
 endif
 
-3dDeconvolve -input r*_scale.nii                            \
+for task in sem plaus gram; do
+
+3dDeconvolve -input *_tag-${task}_*_bold.nii.gz                            \
 #    -censor motion_${subj}_censor.1D                                         \
     -mask full_mask.nii						     \
     -polort 1                                                                \
-    -num_stimts 30                                                           \
-    -fout                                       \
-    -rout                                       \
-    -stim_times 1 stimuli/sem.1D 'BLOCK(2,1)'                          \
-    -stim_label 1 sem                                                  \
-    -stim_times 2 stimuli/sem_ctrl.1D 'BLOCK(2,1)'                        \
-    -stim_label 2 sem_ctrl                                                \
+    -xout -progress                                                     \
+    -num_stimts 12                                                           \
+    -stim_times 1 stimuli/${task}.1D 'BLOCK(2,1)'                          \
+    -stim_label 1 ${task}                                                  \
+    -stim_times 2 stimuli/${task}_ctrl.1D 'BLOCK(2,1)'                        \
+    -stim_label 2 ${task}_ctrl                                                \
     -stim_file 4 regressors/trans_x_${task}_all_tmp.txt'[0]' -stim_base 4 -stim_label 4 trans_x_${task}   \
     -stim_file 5 regressors/trans_y_${task}_all_tmp.txt'[0]' -stim_base 5 -stim_label 5 trans_y_${task}   \
     -stim_file 6 regressors/trans_z_${task}_all_tmp.txt'[0]' -stim_base 6 -stim_label 6 trans_z_${task}   \
@@ -32,13 +31,14 @@ endif
     -stim_file 10 regressors/global_signal_${task}_all_tmp.txt'[0]' -stim_base 10 -stim_label 10 global_signal_${task}     \
     -stim_file 11 regressors/csf_${task}_all_tmp.txt'[0]' -stim_base 11 -stim_label 11 csf_sem     \
     -stim_file 12 regressors/white_matter_${task}_all_tmp.txt'[0]' -stim_base 12 -stim_label 12 white_matter_${task}     \
-    -jobs 8                                                                  \
-    -gltsym 'SYM: ${task} -sem_ctrl'				     \
+    -jobs 8  \
+    # tells AFNI to run GLT based on label names
+    -gltsym 'SYM: ${task} -${task}_ctrl'				     \
     -glt_label 1 ${task} -ctrl					     \
-    -gltsym 'SYM: ${task}_ctrl -sem'				     \
-    -glt_label 2 ctrl -sem					     \
+    -gltsym 'SYM: ${task}_ctrl -${task}'				     \
+    -glt_label 2 ctrl -${task}					     \
   
-    -fout -tout -x1D X.xmat.1D -xjpeg X.jpg                                  \
+    -fout -tout -rout -x1D X.xmat.1D -xjpeg X.jpg                                  \
     -x1D_uncensored X.nocensor.xmat.1D                                       \
     -fitts fitts.$subj                                                       \
     -errts errts.${subj}                                                     \
