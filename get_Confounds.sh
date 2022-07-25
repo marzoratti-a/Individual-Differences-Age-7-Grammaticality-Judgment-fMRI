0b70cb54244682b28d8b8581673deb0a6abae29f
marzoratti-a <107959811+marzoratti-a@users.noreply.github.com>
2022-07-25 15:19:05 -0400

    Update get_Confounds.sh

--- get_Confounds.sh
+++ get_Confounds.sh
@@ -9,6 +9,35 @@ fi
 for subj in `cat subjList.txt`; do
   cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/$subj/ses-7/notspecific
   
+  # https://stackoverflow.com/questions/47056022/how-to-show-filename-when-awk-found-matches
+  grep -ro 'Gram' --include=*.json
+  $ awk '/Sem/' *_regressors.tsv > *_sem_regressors.tsv
+  
+for match in "$(grep -ro 'Sem')";do
+    echo mv "${match%:*.tsv}" "${match#*:}"
+done
+
+# within folder, finds files with json extension and saves name of file as "filename"
+# loops through and if file contains 'Sem', adds "_sem" to corresponding file with filename .tsv 
+
+find -type f -name "*_regressors.json" -print0 | while IFS= read -r -d '' filename
+do
+    if [[grep -q 'Sem' "$filename"]]; then
+        sem_name="${filename%.*}" 
+	mv ${sem_name}.tsv ${sem_name}_sem.tsv
+    fi 
+    
+    if [[grep -q 'Plaus' "$filename"]]; then
+    plaus_name="${filename%.*}"
+    mv ${plaus_name}.tsv ${plaus_name}_plaus.tsv
+    fi
+    
+    if [[grep -q 'Gram' "$filename"]]; then
+        gram_name="${filename%.*}"
+	mv ${gram_name}.tsv ${gram_name}_gram.tsv
+    fi
+done
+  
   # Make txt files with and without header for each regressor of interest
   for reg in global_signal csf white_matter trans_x trans_y trans_z rot_x rot_y rot_z; do
 	  for task in sem plaus gram; do
