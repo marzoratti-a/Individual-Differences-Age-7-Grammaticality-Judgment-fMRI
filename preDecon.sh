7486f459d8884c24c07a4acb1a3ac0b5463a949f
marzoratti-a <107959811+marzoratti-a@users.noreply.github.com>
2022-07-26 22:29:17 -0400

    Update preDecon.sh

--- preDecon.sh
+++ preDecon.sh
@@ -1,14 +1,33 @@
 #!/bin/bash
 
 # first cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload
+# Check whether the file subjList.txt exists; if not, create it
+if [ ! -f subjList.txt ]; then
+	ls | grep ^sub- > subjList.txt
+fi
 
 for subj in `cat subjList.txt`; do
   cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/$subj/ses-7/notspecific
   
+  find . -type f -name "*_regressors.json" -print0 |
+  while IFS= read -r -d '' filename
+  do
+    prefix=${filename%.*}
+    suffix=$(
+        awk '
+            match($0,/Sem|Plaus|Gram/) {
+                print tolower(substr($0,RSTART,RLENGTH))
+                exit
+            }
+        ' "$filename"
+    )
+    mv "$prefix.tsv" "${prefix}_$suffix.tsv" 
+    done
+  
   # Make txt files with and without header for each regressor of interest
   for reg in global_signal csf white_matter trans_x trans_y trans_z rot_x rot_y rot_z; do
 	  for task in sem plaus gram; do
-	    awk -v col=$reg 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' *_${task}_regressors.tsv > ${reg}_${task}_all_tmp.txt;
+	    awk -v col=$reg 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' *_regressors${task}.tsv > ${reg}_${task}_all_tmp.txt;
 	    sed '1d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_noHead_tmp.txt
 	    sed '1!d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_Head_tmp.txt
 	  done
