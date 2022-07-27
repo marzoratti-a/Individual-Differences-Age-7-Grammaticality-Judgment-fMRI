454232dc6c991bfa6e298fe2c35936da69a731c4
marzoratti-a <107959811+marzoratti-a@users.noreply.github.com>
2022-07-26 22:21:45 -0400

    Update get_Confounds.sh

--- get_Confounds.sh
+++ get_Confounds.sh
@@ -2,12 +2,19 @@
 
 cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/sub-5003/ses-7/notspecific
 
-find . -type f -name "*_regressors.json" -print0 | while IFS= read -r -d '' f; do
-    if str=$(grep -wE "Sem|Plaus|Gram" "$f"); then              # search the json file for the strings
-        str=$(head -n 1 <<< "$str" | tr [:upper:] [:lower:])    # pick the 1st match and lower the case
-        base=${f%.json}                                         # remove the extention
-        echo mv -- "${base}.tsv" "${base}_${str}.tsv"           # rename the file
-    fi
+find . -type f -name "*_regressors.json" -print0 |
+while IFS= read -r -d '' filename
+do
+    prefix=${filename%.*}
+    suffix=$(
+        awk '
+            match($0,/Sem|Plaus|Gram/) {
+                print tolower(substr($0,RSTART,RLENGTH))
+                exit
+            }
+        ' "$filename"
+    )
+    mv "$prefix.tsv" "${prefix}_$suffix.tsv" 
 done
   
   
