29419ad85c60f4a35aba8f91d7013a7759b4a137
marzoratti-a <107959811+marzoratti-a@users.noreply.github.com>
2022-08-04 13:34:54 -0400

    Update get_Confounds.sh

--- get_Confounds.sh
+++ get_Confounds.sh
@@ -2,20 +2,25 @@
 
 # first cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload
 
+if [ ! -f subjList.txt ]; then
+	ls | grep ^sub- > subjList.txt
+fi
 
-  cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/sub-5036/ses-7/notspecific
+#Loop over all subjects and format timing files into FSL format
+for subj in `cat subjList.txt`; do
+	cd $subj/ses-7/func/confounds
+
+  #cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/sub-5036/ses-7/notspecific
   
   # Make txt files with and without header for each regressor of interest
-  for reg in global_signal csf white_matter trans_x trans_y trans_z rot_x rot_y rot_z; do
-	  for task in sem plaus gram; do
-	    awk -v col=$reg 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' *_${task}_regressors.tsv > ${reg}_${task}_all_tmp.txt;
-	    sed '1d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_noHead_tmp.txt
-	    sed '1!d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_Head_tmp.txt
+	  for reg in global_signal csf white_matter trans_x trans_y trans_z rot_x rot_y rot_z; do
+		  for task in sem plaus gram; do
+		    awk -v col=$reg 'NR==1{for(i=1;i<=NF;i++){if($i==col){c=i;break}} print $c} NR>1{print $c}' ${task}_regressors.tsv > ${reg}_${task}_all_tmp.txt;
+		    sed '1d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_noHead_tmp.txt
+		    sed '1!d' ${reg}_${task}_all_tmp.txt > ${reg}_${task}_Head_tmp.txt
+		  done
 	  done
-  done
   
-   # Move newly made regressor/confound files to subject folder, rename to regressors
-     cp -avr /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/brainlife.app-fmriprep/sub-5036/ses-7/notspecific /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/sub-5036/ses-7
-     cd /mnt/c/users/anama/onedrive/documents/labwork/proj-62bddf5ef3194eded6f9293d/bids/derivatives/upload/sub-5036/ses-7
-     mv notspecific regressors
+   cd ../../../..
+done
 
